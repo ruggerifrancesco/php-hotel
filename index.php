c1668beff017c306bb690badcb0b5e898e534b28
ruggerifrancesco <lavoro.francescoruggeri@gmail.com>
2023-07-03 16:11:11 +0200

    added style to center table tag

--- index.php
+++ index.php
@@ -50,6 +50,23 @@
     <title>Php Hotel</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" 
         rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+
+    <style>
+        * {
+            outline: 1px solid red;
+        }
+
+        body {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+        }
+
+        table {
+            max-width: 1400px;
+        }
+    </style>
 </head>
 <body>
     <table class="table">
